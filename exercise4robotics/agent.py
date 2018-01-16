5d3ba2e275b8196556807cd254b123ccb11ee6df
RedHeadM <markusmerklinger@Markuss-iMac.local>
2018-01-16 15:55:59 +0100

    early reward stop for episode

--- exercise4robotics/agent.py
+++ exercise4robotics/agent.py
@@ -14,7 +14,7 @@ class DQNAgent:
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
+        self.epsilon = 50.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.99
         self.learning_rate = 0.001
